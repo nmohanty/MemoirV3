047b9b35090ce5b4842a115b5fe8adc26cc48e69
nmohanty <Namrata@Sake.local>
2016-11-30 17:57:02 -0800

    Initial Commit

---
+++ Memoir/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Memoir
+//
+//  Created by Namrata Mohanty on 11/30/16.
+//
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
